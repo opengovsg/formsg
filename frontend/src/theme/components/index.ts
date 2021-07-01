6adc3b01c8630740f08cf9211544a34171219883
chowyiyin <yiyin.chow@gmail.com>
2021-07-01 15:16:13 +0800

    feat: add Tabs style

--- frontend/src/theme/components/index.ts
+++ frontend/src/theme/components/index.ts
@@ -2,5 +2,6 @@ import { Button } from './Button'
 import { Form } from './Form'
 import { FormError } from './FormError'
 import { Input } from './Input'
+import { Tabs } from './Tabs'
 
-export const components = { Button, Input, FormError, Form }
+export const components = { Button, Input, FormError, Form, Tabs }
