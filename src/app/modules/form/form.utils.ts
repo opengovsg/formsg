ecc1d427ba623e771e62a17784a6f4bb9f7623cf
seaerchin <jer005@e.ntu.edu.sg>
2021-07-07 11:09:22 +0800

    fix(verification.controller): shifted order so that retrieveFullForm is awaited on

--- src/app/modules/form/form.utils.ts
+++ src/app/modules/form/form.utils.ts
@@ -128,7 +128,9 @@ export const getLogicById = (
  * @param form The form to check
  * @returns boolean indicating if the form is/is not onboarded
  */
-export const isFormOnboarded = (form: IPopulatedForm): boolean => {
+export const isFormOnboarded = (
+  form: Pick<IPopulatedForm, 'msgSrvcName'>,
+): boolean => {
   return form.msgSrvcName
     ? !(form.msgSrvcName === smsConfig.twilioMsgSrvcSid)
     : false
